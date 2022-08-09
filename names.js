60bcfc469a98e6b12bf27f7bccd301a335515bb3
brianna <BriannasTalkingDrum@gmail.com>
2022-08-09 00:38:16 -0300

    Update names.js

--- names.js
+++ names.js
@@ -1,1 +1,15 @@
+const worker_adjectives = ["Burnt", "Urist", "Fulfilled", "Greasy", "Happy", "Complacent", "Proud", "Violent"]
+const worker_nouns = ["Prole", "Comrade", "Worker"]
 
+
+// Use this for workers
+function genProleName() {
+    return worker_adjectives[Math.floor(Math.random() * worker_adjectives.length)] 
+    + " " // there is a space here its just fucking invisible
+    + worker_nouns[Math.floor(Math.random() * worker_nouns.length)]
+}
+
+module.exports = {
+    worker_adjectives, worker_nouns,
+    genName
+};
