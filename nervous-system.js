12850a23c3e0e08f4019c422cb20346ddb2da5fe
brianna <BriannasTalkingDrum@gmail.com>
2022-08-10 23:11:17 -0300

    Update nervous-system.js
    fixed presets, changed console log

--- nervous-system.js
+++ nervous-system.js
@@ -2,38 +2,40 @@ const minerMax = 5;
 const builderMax = 1;
 const upgraderMax = 1;
 
-
-
-
 console.log("nervous system online..")
 
-const minerPreset = [WORK, CARRY, MOVE]
-const builderPreset = [WORK, CARRY, MOVE]
-const upgraderPreset = [WORK, CARRY, MOVE]
+const creepPresets = {
+    "minerPreset": [WORK, CARRY, MOVE],
+    "builderPreset": [WORK,CARRY, MOVE],
+    "upgraderPreset": [WORK, CARRY, MOVE]
+}
 
 
 function creepSignal(type) {
                     Game.spawns['Spawn1'].
-                    spawnCreep(type + "Preset", 
+                    spawnCreep(creepPresets[type], 
                     Math.floor(Math.random() * 500), 
                     {memory: {role: type}});
                     // Ignore this horrendous formatting for now. :)
-                    console.log("spawning creep with: " + type + "Preset");
+                    console.log("spawning creep with: " + type);
 }
+
+function checkCreeps() {
 
 var numberOfMiners = _.sum(Game.creeps, (c) => c.memory.role == 'miner')
     if (numberOfMiners !== minerMax) {
-       creepSignal("miner");
+       creepSignal("minerPreset");
     }
 
 var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder')
-    if (numberOfMiners !== builderMax) {
-        creepSignal("builder");
+     if (numberOfBuilders !== builderMax) {
+        creepSignal("builderPreset");
     }
 
 var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader')
-    if (numberOfMiners !== upgraderMax) {
-        creepSignal("upgrader");
+    if (numberOfUpgraders !== upgraderMax) {
+        creepSignal("upgraderPreset");
     }
+}
 
-module.exports = { creepSignal }
+module.exports = { creepSignal, checkCreeps }
